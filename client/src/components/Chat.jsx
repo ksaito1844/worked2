5e19c8d8f01a1a96f1b22ea8a656146c8cf39e4a
ksaito1844 <ksaito1844@gmail.com>
2024-08-29 14:15:44 -0400

    Update Chat.jsx

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import io from "socket.io-client";
 
-const socket = io("https://20.119.128.19:5000"); // Replace with your server address
+const socket = io("http://20.119.128.19:5000"); // Replace with your server address
 
 function Chat() {
   const [messages, setMessages] = useState([]);
